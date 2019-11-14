54a487fbed72ce5f2f99ec2ca74134e61c31a81d
as <ankita_sharma1@brown.edu>
2019-11-14 12:39:07 -0500

    next step

--- main.py
+++ main.py
@@ -67,8 +67,6 @@ def connect(hospitals, physicians, patients):
     pa2 = patients[1]
     pa3 = patients[2]
 
-    
-    """
     print("------REGISTRATION")
     pa1.register(h1)
     # Give some time for the blockchain to update.
@@ -76,6 +74,7 @@ def connect(hospitals, physicians, patients):
     pa1.card = None
     print("\n\n------DUP REGISTRATION") 
     pa1.register(h2)
+
 
     pa2.register(h2)
     ph1.register(h2)
@@ -91,7 +90,7 @@ def connect(hospitals, physicians, patients):
     print("\n\n---------> Test that physician can read patient's records")
     # Test that physician can read medical records for patient.
     pa2.read_medical_record(ph1, h2)
-
+    """
     
     print("\n\n---------> Remove patient data")
     # Remove patient data.
@@ -100,7 +99,7 @@ def connect(hospitals, physicians, patients):
     # Try to read data.
     pa2.read(h2)
     """
-
+    """
     print("---------> \n\n Test patient request to transfer")
     pa3.register(h1)
     ph3.register(h1)
@@ -111,7 +110,7 @@ def connect(hospitals, physicians, patients):
     print("---------> Read from h3")
     assert(pa3.transfer(h1, h3) == True)
     pa3.read(h3)
-
+  
     print("---------> \n\n Test that card got updated")
     ph3.register(h3)
     time.sleep(10)
@@ -121,7 +120,7 @@ def connect(hospitals, physicians, patients):
     print("---------> \n\n Test that physician can request to transfer")
     assert(pa3.transfer_medical_record(h3, h2, ph3) == True) 
     pa3.read(h2) 
-
+    """
 """
 Helper function to simulate interactions.
 """
